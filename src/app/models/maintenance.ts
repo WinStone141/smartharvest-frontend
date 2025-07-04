9de0dcbb739ea9e2a590afaa1b419a87787863d8
Emma <138627227+emmatatte@users.noreply.github.com>
2025-07-04 11:47:22 -0500

    correccion maintenance.ts

--- src/app/models/maintenance.ts
+++ src/app/models/maintenance.ts
@@ -1,9 +1,9 @@
 import { Sensor } from "./sensor";
 
-export class Maintenance{
-idMaintenance: number = 0;
-installationDate: Date = new Date();
-tipoMantenimiento: string = ""; //50 caracteres
-description: string = "";  // 200 caracteres
-sensor: Sensor = new Sensor ();
+export class Maintenance {
+    idMaintenance: number = 0;
+    installationDate: Date = new Date();
+    tipoMantenimiento: string = "";
+    description: string = "";
+    sensor: Sensor = new Sensor();
 }
